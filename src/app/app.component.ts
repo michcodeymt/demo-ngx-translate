8f731077df614af707e28a800c813726d893472b
Marketing L&C <ymontanez@nvo.pe>
2020-08-21 21:35:33 -0500

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {I18nServiceService} from './i18n-service/i18n-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngx-translate-demo';
+
+  constructor(
+    translate: TranslateService,
+    private i18nService: I18nServiceService
+  ) {
+    translate.setDefaultLang('en');
+    translate.use('ar');
+  }
+
+  changeLocale(locale: string) {
+    this.i18nService.changeLocale(locale);
+  }
 }
